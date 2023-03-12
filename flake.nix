fd21dd20aa00726a2dfab187a9b8953a903acd24
Thomas Boerger <thomas@webhippie.de>
2023-03-12 21:15:47 +0100

    chore: integrate nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gnumake
+            go_1_19
+          ];
+        };
+      }
+    );
+}
