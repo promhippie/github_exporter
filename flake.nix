f33559c7a84ad33f597be2be998c47af9d35dcef
Thomas Boerger <thomas@webhippie.de>
2023-08-10 14:07:21 +0200

    ci: use new flake-utils

--- flake.nix
+++ flake.nix
@@ -3,26 +3,44 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    utils = {
-      url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
     };
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            gnumake
-            go_1_19
-          ];
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devshells = {
+          default = {
+            motd = ''
+              {202}🔨 Welcome to devshell{reset}
+            '';
+
+            commands = [
+
+            ];
+
+            packages = with pkgs; [
+              gnumake
+              go
+            ];
+          };
         };
-      }
-    );
+      };
+    };
 }
