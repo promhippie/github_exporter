896c2e1f468cda4cc56a3045283408cb58d0abeb
Thomas Boerger <thomas@webhippie.de>
2024-07-24 09:30:12 +0200

    ci: add flake workflow and update flakes

--- flake.nix
+++ flake.nix
@@ -6,15 +6,24 @@
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    devshell = {
-      url = "github:numtide/devshell";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
     };
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.devshell.flakeModule
+        inputs.devenv.flakeModule
+        inputs.pre-commit-hooks-nix.flakeModule
       ];
 
       systems = [
@@ -25,23 +34,48 @@
       ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devshells = {
-          default = {
-            motd = ''
-              {202}🔨 Welcome to devshell{reset}
-            '';
+        imports = [
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        ];
 
-            commands = [
+        pre-commit = {
+          settings = {
+            hooks = {
+              nixpkgs-fmt = {
+                enable = true;
+              };
+              golangci-lint = {
+                enable = true;
+              };
+            };
+          };
+        };
 
-            ];
+        devenv = {
+          shells = {
+            default = {
+              languages = {
+                go = {
+                  enable = true;
+                  package = pkgs.go_1_22;
+                };
+              };
 
-            packages = with pkgs; [
-              gnumake
-              go
-              hugo
-              podman-compose
-              sqlite
-            ];
+              packages = with pkgs; [
+                bingo
+                gnumake
+                nixpkgs-fmt
+              ];
+
+              env = {
+                CGO_ENABLED = "0";
+              };
+            };
           };
         };
       };
